ff0c50415022d904a3641deda377cb70a1aa640e
Hugo Campos <85531201+HC-85@users.noreply.github.com>
2024-08-06 16:38:50 +0000

    removed unnecessary files

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
+from sklearn.linear_model import LinearRegression
+from datetime import datetime as dt 
 from flask import Flask, request, jsonify
-import joblib
 import numpy as np
 
 app = Flask(__name__)
 
-model = joblib.load('linear_regression_model.pkl')
-
-@app.route('/predict', methods=['POST'])
-def predict():
+@app.route('/suggest_savings', methods=['POST'])
+def suggest_savings():
     data = request.get_json(force=True)
-    X_new = np.array(data['input']).reshape(-1, 1)
-    prediction = model.predict(X_new)
-    return jsonify({'prediction': prediction.tolist()})
+    X = np.array(data['X'])
+    y = np.array(data['y'])
+
+    model = LinearRegression()
+    model.fit(X.reshape(-1, 1), y)
+
+    dinero_meta = data['dinero_meta']
+    fecha_meta = dt.strptime(data['fecha_meta'], '%d-%m-%Y').toordinal()
+
+    dinero_faltante = dinero_meta - y[-1]
+    dias_faltantes = fecha_meta - X[-1]
+
+    ahorro_extra_mensual_necesario = (dinero_faltante/dias_faltantes - model.coef_)*30
+
+    return jsonify({'ahorro_extra_mensual_necesario': ahorro_extra_mensual_necesario.item()})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
